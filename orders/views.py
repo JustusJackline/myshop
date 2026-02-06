6173272ff1e98a0143b088d76cb33562c52def55
JustusJackline <justusjackline1@gmail.com>
2026-02-06 16:22:48 +0300

    set up celery with RabbitMQ for background tasks

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .task import order_created
 
 
 
@@ -18,6 +19,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            order_created.delay(order.id)
             return render(request,'orders/order/created.html',{'order':order})
     else:
         form=OrderCreateForm()
